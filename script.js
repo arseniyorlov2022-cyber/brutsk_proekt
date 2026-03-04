6ea40792a0a89e37f8d4af7c248bff124281164e
arseniyorlov2022-cyber <arseniy.orlov.2022@gmail.com>
2026-03-04 19:45:12 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+<div class="carousele">
+    <div class="carousele-element">
+      <img src="https://i.postimg.cc/0NfnDfqy/image.jpg" width="600">
+    </div>
+    <div class="carousele-element">
+      <img src="https://i.postimg.cc/HWK2wnFX/image.jpg" width="600">
+    </div>
+    <div class="carousele-element">
+      <img src="https://i.postimg.cc/QdZJzP8M/image.jpg" width="600">
